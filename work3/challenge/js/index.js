96ceae61c7eb3d4d63ae3d905d88c868a3527c17
sawako <kowasa88@gmail.com>
2023-05-06 08:08:11 +0900

    feat: Add the hamburger menu for mobile devices

--- work3/challenge/js/index.js
+++ work3/challenge/js/index.js
@@ -14,7 +14,7 @@ for (let i = 0; i < main.length; i++) {
 // Open the dropdown menu
 function dropdownOpen(element) {
     const dropdownLink = element.currentTarget.querySelectorAll('.drop-menu-link');
-    console.log(dropdownLink);
+    // console.log(dropdownLink);
 
     for (let i = 0; i < dropdownLink.length; i++) {
         dropdownLink[i].classList.add('open');
@@ -29,4 +29,17 @@ function dropdownClose(element) {
         dropdownLink[i].classList.remove('open');
     }
 }
+
+// hamburger menu
+const ham = document.querySelector('#js-hamburger');
+const nav = document.querySelector('#js-nav');
+
+ham.addEventListener('click', function(){
+    // classListプロパティは、特定の要素のクラス名を追加・削除・参照できる
+    // classListの主なメソッドは、add, remove, contains, toggle(クラスが含まれていなければ追加, and vice versa)
+    // 参考： https://qiita.com/tomokichi_ruby/items/2460c5902d19b81cace5
+    ham.classList.toggle('active');
+    nav.classList.toggle('active');
+})
+
 }
