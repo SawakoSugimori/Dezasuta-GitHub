dd49b7378a74cee6383b03f91e89cfa73dcf234f
sawako <kowasa88@gmail.com>
2023-07-18 12:36:04 +0900

    Separate the header

--- portfolio_php/work-deatils/work_mobile.php
+++ portfolio_php/work-deatils/work_mobile.php
@@ -16,70 +16,18 @@
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&family=Poppins:wght@400;500&family=Ubuntu&display=swap');
     </style>
+    <!-- php -->
+    <?php
+    $path = '../'; // ファイルパスの変数
+    ?>
+
 </head>
 
 <body>
-    <header id="header">
-        <div class="header_wrap">
-            <nav class="global_nav">
-                <ul class="nav_left">
-                    <li><a href="../index.html">TOP</a></li>
-                    <li><a href="../work/index.html">WORK</a></li>
-                    <li><a href="../about/index.html">ABOUT</a></li>
-                    <li><a href="../contact/index.html">CONTACT</a></li>
-                </ul>
-                <div class="nav_right">
-                    <div class="lang_switcher">
-                        <p class="lang"><a href="#">日本語</a></p>
-                        <span>/</span>
-                        <p class="lang"><a href="#">English</a></p>
-                    </div>
-                    <ul class="social_links">
-                        <li class="social_icon"><a href="#"><img src="../images/icons/github-icon-min.png" alt="
-                                    Github"></a>
-                        </li>
-                        <li class="social_icon"><a href="#"><img src="../images/icons/ig-icon-min.png" alt="
-                                    Instagram"></a>
-                        </li>
-                        <li class="social_icon"><a href="#"><img src="../images/icons/twitter-icon-min.png" alt="
-                                    Twitter"></a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <!-- hamburger menu-->
-            <div class="fixed">
-                <div class="ham_btn"><span></span><span></span></div>
-            </div>
-            <nav class="sp_nav null">
-                <div class="close_btn"><span></span><span></span></div>
-                <ul class="nav_left">
-                    <li><a href="../index.html">TOP</a></li>
-                    <li><a href="../work/index.html">WORK</a></li>
-                    <li><a href="../about/index.html">ABOUT</a></li>
-                    <li><a href="../contact/index.html">CONTACT</a></li>
-                </ul>
-                <div class="nav_right">
-                    <ul class="social_links">
-                        <li class="social_icon"><a href="#"><img src="../images/icons/github-white-64af4ba148b4e.webp"
-                                    alt="Github"></a>
-                        </li>
-                        <li class="social_icon"><a href="#"><img src="../images/icons/ig-white-64af4ba154069.webp"
-                                    alt="Instagram"></a>
-                        </li>
-                        <li class="social_icon"><a href="#"><img src="../images/icons/twitter-white-64af4ba3dc597.webp"
-                                    alt="Twitter"></a>
-                        </li>
-                    </ul>
-                    <div class="lang_switcher">
-                        <p class="lang"><a href="#">日本語</a></p>
-                        <span>/</span>
-                        <p class="lang"><a href="#">English</a></p>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </header>
+    <!-- Include header.php -->
+    <?php include 
+    $path.'inc/header.php';
+    ?>
     <section class="work_details">
         <div class="banner_mockup">
             <img src="../work/images/banner-mobile-80-64acd75b13165.webp" alt="バナーの写真">
