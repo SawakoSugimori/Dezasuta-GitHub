a8cd5b73e7b21c8c55e82b07f9ef269be082e149
sawako <kowasa88@gmail.com>
2023-07-19 12:03:04 +0900

    Fix: Fix to add a class value properly

--- portfolio_php/inc/header.php
+++ portfolio_php/inc/header.php
@@ -9,9 +9,9 @@
                 </ul>
                 <div class="nav_right">
                     <div class="lang_switcher">
-                        <p class="lang" <?php echo isset($is_ja) ? 'class="current_page"': ''?>><a href="<?php echo isset($is_ja)? '': $ja_page;?>">日本語</a></p>
+                        <p class="lang <?php echo isset($is_ja) ? 'current_page': ''?>"><a href="<?php echo isset($is_ja)? '': $ja_page;?>">日本語</a></p>
                         <span>/</span>
-                        <p class="lang" <?php echo isset($is_en) ? 'class="current_page"': ''?>><a href="<?php echo isset($is_en)? '': $en_page;?>">English</a></p>
+                        <p class="lang <?php echo isset($is_ja) ? 'current_page': ''?>"><a href="<?php echo isset($is_en)? '': $en_page;?>">English</a></p>
                     </div>
                     <ul class="social_links">
                         <li class="social_icon"><a href="#"><img src="<?php echo $path;?>/images/icons/github-icon-min.png"
