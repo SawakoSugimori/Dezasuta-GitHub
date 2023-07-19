c9f14b9627461503af96aeda4838bad2ac66b414
sawako <kowasa88@gmail.com>
2023-07-19 09:30:10 +0900

    First Commit: Create english pages

---
+++ portfolio_php/contact/index_en.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CONTACT :Sawako Sugimori's Portfolio</title>
+    <meta name="description" content="杉森佐和子へのお問い合わせページです。">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../css/commons.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- jQuery CDN -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+
+    <!-- Google Fonts -->
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&family=Poppins:wght@400;500&family=Ubuntu&display=swap');
+    </style>
+    <!-- php -->
+    <?php
+    $path = '../'; // ファイルパスの変数
+    $is_contact = true; // 現在の位置
+    ?>
+</head>
+
+<body>
+    <!-- Include header.php -->
+    <?php include 
+    $path.'inc/header.php';
+    ?>
+
+    <section class="contact">
+        <h1 class="page_title">CONTACT</h1>
+        <p>ご依頼・ご質問等がありましたら、お問い合わせフォームやSNSより、お気軽にお問い合わせください。</p>
+        <ul class="social_links">
+            <li class="social_icon"><a href="#"><img src="../images/icons/ig-icon-min.png" alt="
+                Instagram"></a>
+            </li>
+            <li class="social_icon"><a href="#"><img src="../images/icons/twitter-icon-min.png" alt="
+                Twitter"></a>
+            </li>
+
+        </ul>
+        <form
+            action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSfW3Gw83pX6i_pLZpG5GUARikziPbYqu8m0Eiq6oGSUUvM6Yw/formResponse"
+            method="post" target="hidden_iframe" onsubmit="submitted=true">
+
+            <label for="name">お名前</label>
+            <input id="name" class="required" type="text" placeholder="山田 太郎" name="entry.1982888611" required>
+            <p class="error_text"></p>
+
+            <label for="email">メールアドレス</label>
+            <input id="email" class="required" type="text" placeholder="example@mail.co.jp" name="entry.463273501"
+                required>
+            <p class="error_text"></p>
+
+            <label for="msg">メッセージ</label>
+            <textarea id="msg" class="required" placeholder="お気軽にお問い合わせください。" name="entry.1729223197"
+                required></textarea>
+            <p class="error_text"></p>
+
+            <!-- <p class="submit">submit</p> -->
+            <button class="submit" type="submit" name="button" value="送信">メッセージを送信する</button>
+        </form>
+    </section>
+
+    <!-- Footer -->
+    <?php include 
+    $path.'inc/footer.php';
+    ?>
+    <!-- jQuery -->
+    <script>
+        var submitted = false;
+        $(function () {
+            // Form Validation 
+            // Get values of the input field with id="required"
+            let requireds = document.querySelectorAll('.required');
+            // エラーメッセージを表示するpタグを取得
+            let errors = document.querySelectorAll('.error_text');
+            // 必須フィールドの数を取得
+            let requiredLength = requireds.length;
+            // sunmitボタンを取得
+            const submit = document.querySelector('.submit');
+            submit.addEventListener('click', function (e) {
+                // Cancel default actions
+                for (let i = 0; i < requiredLength; i++) {
+                    if (requireds[i].value == "") {
+                        // エラーメッセージを表示
+                        errors[i].innerHTML = "このフィールドは必須です。";
+                    } else {
+                        errors[i].innerHTML = "";
+                    }
+                }
+            });
+        });
+
+    </script>
+    <!-- Render to the thank you page-->
+    <iframe name="hidden_iframe" id="hidden_iframe" style="display: none;"
+        onload="if(submitted) {window.location='thanks.php';}">
+    </iframe>
+</body>
+
+</html>
---
+++ portfolio_php/contact/thanks_en.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thank you for reaching out to me! :Sawako Sugimori's Portfolio</title>
+    <meta name="description" content="杉森佐和子へのお問い合わせページです。">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../css/commons.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- jQuery CDN -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+
+    <!-- Google Fonts -->
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&family=Poppins:wght@400;500&family=Ubuntu&display=swap');
+    </style>
+    <!-- php -->
+    <?php
+    $path = '../'; // ファイルパスの変数
+    ?>
+</head>
+
+<body>
+    <!-- Include header.php -->
+    <?php include 
+    $path.'inc/header.php';
+    ?>
+
+    <section class="thankyou">
+        <div class="left">
+            <div class="bouquet sp_only">
+                <img src="./image/thanks_2-min.png" alt="イラスト">
+            </div>
+            <h1 class="page_title">thank you<span>!</span></h1>
+            <div class="text">
+                <p>お問い合わせありがとうございます！お送り頂きました内容を確認の上、3日以内に折り返しご連絡させて頂きます。</p>
+                <p class="caution">
+                    *3日が過ぎてもメールが届かない場合は、入力頂いたメールアドレスが間違っているか、迷惑メールフォルダに振り分けられている可能性がございます。また、ドメイン指定をされている場合は、「sawakosugimori@gmail.com」からのメールが受信できるようあらかじめ設定をお願いいたします。
+                </p>
+            </div>
+            <div class="btn_block">
+                <a class="btn" href="../index.php">トップページに戻る</a>
+            </div>
+        </div>
+        <div class="right pc_only">
+            <img src="./image/thanks_2-min.png" alt="イラスト">
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <?php include 
+    $path.'inc/footer.php';
+    ?>
+    <script>
+    </script>
+</body>
+
+</html>
