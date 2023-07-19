286d9e0abfda19a0848dc49cda0751ce387bcd48
sawako <kowasa88@gmail.com>
2023-07-19 13:25:32 +0900

    Delete a contact button

--- portfolio_php/inc/footer.php
+++ portfolio_php/inc/footer.php
@@ -16,11 +16,5 @@
         </div>
     </div>
 </footer>
-<!-- contact button -->
-<div class="to_contact">
-    <button class="contact_pop" href="<?php echo $path;?>contact/index<?php echo isset($is_en)? '_en': ''?>.php">
-        <img src="<?php echo $path;?>images/contact-button.png" alt="CONTACT">
-    </button>
- </div>
 <!-- Common js -->
 <script src="<?php echo $path;?>js/script.js"></script>
