b57363221d7374676c42a3496d04e48dd3e6b4e5
sawako <kowasa88@gmail.com>
2023-07-19 10:10:33 +0900

    Translate head to English

--- portfolio_php/work/index.php
+++ portfolio_php/work/index.php
@@ -21,6 +21,8 @@
     <?php
     $path = '../'; // ファイルパスの変数
     $is_work = true; // 現在の位置
+    $is_ja = true; // Current language
+    $en_page = './index_en.php'; // To English page
     ?>
 </head>
 
