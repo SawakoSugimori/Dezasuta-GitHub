90b460a05e9993e44fb7a24f0bae961b8679eb31
sawako <kowasa88@gmail.com>
2023-07-19 11:35:02 +0900

    Fix: Translate an alt content to English

--- portfolio_php/index_en.php
+++ portfolio_php/index_en.php
@@ -65,7 +65,7 @@
                     <div class="inner"><!-- ここにもposition: absoluteを使用-->
                         <div class="circle"></div>
                         <div class="my_pic">
-                            <img src="./images/profilepic-min.png" alt="杉森佐和子の写真">
+                            <img src="./images/profilepic-min.png" alt="Profile Picture">
                         </div>
                         <h2 class="sp_only">Hello<span>!</span></h2>
                     </div>
